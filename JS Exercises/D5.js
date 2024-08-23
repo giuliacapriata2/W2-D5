7cb47eb275b8e9c64a61319eb546d24a64e275f0
Giulia Capriata <giuliacapriata2@gmail.com>
2024-08-23 11:43:50 +0200

    5

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -64,6 +64,12 @@ const cars = [
   },
 ]
 
+for (let i = 0; i < cars.length; i++) {
+  cars[i].licensePlate = 'ABC759';
+}
+console.log(cars)
+
+
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
